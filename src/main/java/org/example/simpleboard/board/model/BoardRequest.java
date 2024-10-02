b4edc88047ca2a3312a27c8da0a979bde993633d
gudae01 <guseorn@gmail.com>
2024-10-02 15:22:50 +0900

    update

--- src/main/java/org/example/simpleboard/board/model/BoardRequest.java
+++ src/main/java/org/example/simpleboard/board/model/BoardRequest.java
@@ -1,10 +1,15 @@
 package org.example.simpleboard.board.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
